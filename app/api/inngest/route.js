a7aeb6517ce4d2693584c6d559a0a82a67b25b78
ayushi2910 <itsayushiranjan@gmail.com>
2025-07-05 06:04:37 +0530

    inngest invoked

---
+++ app/api/inngest/route.js
@@ -0,0 +1,23 @@
+import { serve } from "inngest/next";
+
+import { inngest } from "@/lib/inngest/client";
+import {
+ 
+  checkBudgetAlerts,
+  generateMonthlyReports,
+  processRecurringTransaction,
+  triggerRecurringTransactions,
+
+  
+} from "@/lib/inngest/function";
+
+export const { GET, POST, PUT } = serve({
+  client: inngest,
+  functions: [
+  
+    checkBudgetAlerts,
+    triggerRecurringTransactions,
+    processRecurringTransaction,
+    generateMonthlyReports
+  ],
+});
